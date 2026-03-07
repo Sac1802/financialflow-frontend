cd69b1e191fc4bea8739994fea73585d23966729
Said <saidacostacepeda03@gmail.com>
2026-03-06 23:12:45 -0500

    feat: add service for register user

---
+++ src/app/core/interfaces/user.register.interface.ts
@@ -0,0 +1,5 @@
+export interface userRegister{
+    name: string;
+    email: string;
+    password: string;
+}
---
+++ src/app/core/services/user-create.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, signal, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, Observable, tap, throwError } from 'rxjs';
+import { userRegister } from '../interfaces/user.register.interface';
+import { environment } from '../../../environments/environment';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class userCreateSercice {
+  private http = inject(HttpClient);
+  private router = inject(Router);
+  createdSuccess = signal<boolean>(false);
+
+  createUser(data: userRegister) {
+    return this.http.post<string>(environment.apiUrl, data).pipe(
+      tap((res) => {
+        this.createdSuccess.set(true);
+        this.router.navigate(['login']);
+      }),
+      catchError((err) => {
+        console.error('Error login', err);
+        return throwError(() => err);
+      }),
+    );
+  }
+}
